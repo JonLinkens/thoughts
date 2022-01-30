91b0d543bbd0d7200e4172a6b3c964ac4aa7a598
Jon Linkens <jonlinkens@outlook.com>
2022-01-30 19:58:32 +0000

    update date

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
     <div className="mt-4 mb-2 select-none mx-4 md:text-md text-gray-900 text-sm text-center">
       <div className="">{"</> with 	♥"}</div>
       <a href="https://www.jonlinkens.com/" className="mt-2 mb-4">
-        Jon Linkens 2021
+        Jon Linkens 2022
       </a>
     </div>
   );
